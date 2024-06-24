adcdce33daa202f0da57c6480b39ec5e1b97e6d1
VasiliySiniavin <vasinyavin@yandex.ru>
2024-06-24 21:33:41 +0300

    first commit with created class startpage

---
+++ src/test/java/pages/start_page/StartPage.java
@@ -0,0 +1,86 @@
+package pages.start_page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+
+public class StartPage {
+
+    SelenideElement formAuth = $x("//app-login-form/ui-form");
+    SelenideElement logo = $x("//div/img");
+    SelenideElement loginText = $x("//*[@id=\"username\"]/div/ui-field-title");
+    SelenideElement login = $(By.id("username"));
+    SelenideElement passwordText = $x("//*[@id=\"password\"]/div/ui-field-title");
+    SelenideElement password = $(By.id("password"));
+    SelenideElement errorText = $x("//ui-field-error");
+    SelenideElement passwordUnmask = $x("//label/button");
+    SelenideElement enterButton = $x("//div/button");
+    SelenideElement forgotPassword = $x("//div/a");
+
+    @Step("Проверка отображения формы авторизации")
+    public boolean getFromAuthorization() {
+        return formAuth.shouldBe(Condition.visible).isDisplayed();
+    }
+    @Step("Проверка заголовка страницы")
+    public String getTitlePage() {
+        return title();
+    }
+    @Step("Проверка отображения Логотипа")
+    public boolean getLogo() {
+        return logo.shouldBe(Condition.visible).isDisplayed();
+    }
+    @Step("Проверка текста поля Логин")
+    public boolean getLoginText() {
+        return loginText.shouldBe(Condition.visible).isDisplayed();
+    }
+    @Step("Проверка наличия поля Пароль")
+    public boolean getLogin() {
+        return login.shouldBe(Condition.visible).isDisplayed();
+    }
+    @Step("Установить значение в поле Логин")
+    public void setLogin(String strLogin) {
+        login.setValue(strLogin).pressEnter();
+    }
+    @Step("Проверка текста поля Пароль")
+    public boolean getPasswordText() {
+        return passwordText.shouldBe(Condition.visible).isDisplayed();
+    }
+    @Step("Проверка наличия поля Пароль")
+    public boolean getPassword() {
+        return password.shouldBe(Condition.visible).isDisplayed();
+    }
+    @Step("Установить значение в поле Пароль")
+    public void setPassword(String strPassword) {
+        login.setValue(strPassword).pressEnter();
+    }
+    @Step("Получить текст ошибки")
+    public String getErrorText() {
+        return errorText.shouldBe(Condition.visible).getText();
+    }
+    @Step("Нажать на кнопку 'Пароль'")
+    public void getPasswordUnmask() {
+        passwordUnmask.shouldBe(Condition.visible).click();
+    }
+    @Step("Проверка наличия кнопки 'Войти'")
+    public boolean getEnterButton() {
+        return enterButton.exists();
+    }
+    @Step("Нажать на кнопку 'Войти'")
+    public void clickEnterButton() {
+        enterButton.shouldBe(Condition.enabled).click();
+    }
+    @Step("Проверка наличия гиперссылки 'Забыли пароль?'")
+    public boolean getForgotPassword() {
+        return forgotPassword.shouldBe(Condition.visible).isDisplayed();
+    }
+
+    @Step("Ввод авторизационных данных и нажатие на кнопку 'Войти'")
+    public void enterAuthorizationData(String strLogin, String strPassword) {
+        this.setLogin(strLogin);
+        this.setPassword(strPassword);
+        this.clickEnterButton();
+    }
+}
+
---
+++ src/test/java/tests/start_page/StartPage_Test.java
@@ -0,0 +1,33 @@
+package tests.start_page;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import com.codeborne.selenide.Configuration;
+import static com.codeborne.selenide.Selenide.*;
+import io.qameta.allure.*;
+import pages.start_page.StartPage;
+
+
+@Epic("Тестирование Cтартовой страницы")
+public class StartPage_Test extends StartPage {
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://partner.agentapp.ru/";
+        Configuration.timeout = 10000;
+        open(Configuration.baseUrl);
+    }
+
+    @Feature("Заголовок, логотип")
+    @Test
+    @Description("Позитивный. Проверка заголовка страницы, логотипа")
+    public void testCheckTitilePage() {
+        String actualResult = getTitlePage();
+        String expectedResult = "AgentApp";
+        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertTrue(getLogo());
+    }
+}
