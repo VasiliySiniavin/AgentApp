a41ceec176bb498fd78512bac5df4b4c1cc719e2
VasiliySiniavin <vasinyavin@yandex.ru>
2024-06-26 18:37:38 +0300

    Debugging build~2

--- src/test/java/tests/start_page/StartPage_Test.java
+++ src/test/java/tests/start_page/StartPage_Test.java
@@ -23,7 +23,7 @@ public class StartPage_Test extends StartPage {
         Configuration.baseUrl = "https://partner.agentapp.ru/";
         Configuration.timeout = 10000;
         Configuration.browserCapabilities.setCapability("goog:chromeOptions", new HashMap<String, Object>() {{
-            put("args", Arrays.asList("--disable-dev-shm-usage", "--no-sandbox", "--disable-gpu", "--disable-extensions", "--remote-allow-origins=*"));
+            put("args", Arrays.asList("--disable-dev-shm-usage", "--no-sandbox", "--disable-gpu", "--remote-allow-origins=*"));
         }});
         open(Configuration.baseUrl);
     }
